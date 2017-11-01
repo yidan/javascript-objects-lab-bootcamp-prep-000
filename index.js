f4b5693d33740e13b9d4cf1f6d47ed6ba831d3c9
Yidan Wang <wangyidan621@gmail.com>
2017-11-01 18:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value){
   Object.assign({},object,{[key]:value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var temp = Object.assign({},object)
+  delete temp[key]
+  return temp
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
+
